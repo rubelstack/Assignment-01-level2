24483f7cdb044f27105b3066025d5340988d2aef
rubelstack <ariyankhanoyon396@gmail.com>
2026-09-02 02:13:19 +0600

    removes clg

--- problem-04.js
+++ problem-04.js
@@ -7,11 +7,3 @@ return isNight ? fare * 1.2 : fare;
 
 
 }
-
-console.log(getCngFare(2));
-console.log(getCngFare(1));
-console.log(getCngFare(5));
-console.log(getCngFare(10));
-console.log(getCngFare(5, false, 10));
-console.log(getCngFare(5, true));
-console.log(getCngFare(5, true, 10));
