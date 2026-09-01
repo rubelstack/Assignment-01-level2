bc5fcd83b48982e1653f09f5789fec3ea925b4f6
rubelstack <ariyankhanoyon396@gmail.com>
2026-09-01 21:06:16 +0600

    using prettier to making look great and removing log

--- problem-02.js
+++ problem-02.js
@@ -1,22 +1,15 @@
 const getDayType = (day) => {
   switch (day.toLowerCase()) {
-    case "saturday": case "friday": return `Weekend`;
-    case "sunday": case "monday": case "tuesday": case "wednesday": case "thursday":
+    case "saturday":
+    case "friday":
+      return `Weekend`;
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
       return `Working Day`;
     default:
       return "Invalid Day";
   }
 };
- console.log(getDayType("Friday")); 
-console.log(getDayType("friday")); 
-console.log(getDayType("FRIDAY")); 
-console.log(getDayType("Saturday"));
-
-console.log(getDayType("Sunday"));     
-console.log(getDayType("MONDAY"));     
-console.log(getDayType("tuesday"));    
-console.log(getDayType("Wednesday"));  
-console.log(getDayType("THURSDAY"));   
-
-console.log(getDayType("Bandarban"));  
-console.log(getDayType("Holiday"));    
