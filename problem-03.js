e3df58931391fc9498642984eeed47564e232d2a
rubelstack <ariyankhanoyon396@gmail.com>
2026-09-01 22:08:01 +0600

    ptoblem3 fixed

---
+++ problem-03.js
@@ -0,0 +1,12 @@
+const validateUsername = (username) => {
+return username.length < 4  ? "Too Short" :  username.includes(" ")
+      ? "No Space Allowed" : username.toLowerCase().includes("admin")
+        ? "Reserved Word" : "Available";
+};
+console.log(validateUsername("rahim123"));
+console.log(validateUsername("ab"));
+console.log(validateUsername("a b"));
+console.log(validateUsername("abcd"));
+console.log(validateUsername("rahim islam"));
+console.log(validateUsername("superadmin99"));
+console.log(validateUsername("Admin_Rahim"));
